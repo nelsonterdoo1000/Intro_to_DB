26e03b43c632c663c0187fc201555eaaea7b70af
Terdoo Nelson <nelsonterdoo1000@gmail.com>
2025-06-29 23:18:16 +0100

    edited the code to suit what the checker is looking for

--- MySQLServer.py
+++ MySQLServer.py
@@ -47,7 +47,7 @@ def create_database():
             
             print("Database 'alx_book_store' created successfully!")
             
-    except Error as e:
+    except mysql.connector.Error as e:
         print(f"Error: Failed to connect to MySQL server or create database")
         print(f"MySQL Error: {e}")
         
